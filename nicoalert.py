a4c0dad8903e41110ebc892727d1e3f0abebc33a
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2013-10-24 04:01:47 +0900

    Applied PEP8.

--- nicoalert.py
+++ nicoalert.py
@@ -7,7 +7,7 @@ import os
 import sys
 import ConfigParser
 import logging
-import logging.config 
+import logging.config
 import datetime
 import urllib
 import urllib2
@@ -19,11 +19,7 @@ import tweepy
 
 NICOALERT_CONFIG = os.path.dirname(os.path.abspath(__file__)) + '/nicoalert.config'
 
-# test data
-test_stream_info_normal = '<getstreaminfo status="ok"><request_id>lv102363764</request_id><streaminfo><title>&#12304;&#36196;&#20154;&#12305;&#12496;&#12452;&#12488;&#12414;&#12391;&#12510;&#12452;&#12531;&#12463;&#12521;&#12501;&#12488;&#12304;&#12450;&#12459;&#12488;</title><description>&#21021;&#24515;&#32773;&#12391;&#12377;(&#180;&#12539;&#969;&#12539;`)&#33394;&#12293;&#12431;&#12363;&#12425;&#12394;&#12356;&#12371;&#12392;&#12354;&#12426;&#12377;&#12366;&#12390;&#12290;&#12290;&#12290;&#12522;&#12473;&#12490;&#12540;&#12373;&#12435;&#12395;&#21161;&#12369;&#12425;&#12428;&#12390;&#12414;&#12377;&#65367;&#65367;&#65367;&#12424;&#12363;</description><provider_type>community</provider_type><default_community>co1684688</default_community></streaminfo><communityinfo><name>&#36196;&#20154;&#12398;&#12418;&#12385;&#12419;&#12418;&#12385;&#12419;&#37197;&#20449;&#65288; &#12387; &#180; &#969; &#65344; &#65347; &#65289;</name><thumbnail>http://icon.nimg.jp/community/s/168/co1684688.jpg?1341243427</thumbnail></communityinfo><adsense><item><name>&gt;&gt;&#12491;&#12467;&#29983;&#12463;&#12523;&#12540;&#12474;&#12391;&#20182;&#12398;&#30058;&#32068;&#12434;&#25506;&#12377;</name><url>http://live.nicovideo.jp/cruise</url></item></adsense></getstreaminfo>'
-test_stream_info_no_title = '<getstreaminfo status="ok"><request_id>lv102363769</request_id><streaminfo><title/><description>&#12424;&#12369;&#12428;&#12400;&#12289;&#12467;&#12513;&#12531;&#12488;&#12394;&#12393;&#12394;&#12393;&#12375;&#12390;&#12356;&#12387;&#12390;&#12367;&#12384;&#12373;&#12356;&#12394;&#65367;&#65367;&#65367;&#65367;&#25237;&#31295;&#21205;&#30011;&#12539;http://www.nicovideo.jp/watch/13</description><provider_type>community</provider_type><default_community>co1645552</default_community></streaminfo><communityinfo><name>&#9734;&#65302;&#21495;&#12392;&#24841;&#24555;&#12394;&#12522;&#12473;&#12490;&#12540;&#36948;&#12392;&#12381;&#12398;&#20182;&#12418;&#12429;&#12418;&#12429;&#12398;&#38598;&#12356;&#12398;&#22580;&#9734;</name><thumbnail>http://icon.nimg.jp/community/s/164/co1645552.jpg?1337421832</thumbnail></communityinfo><adsense><item><name>&gt;&gt;&#12491;&#12467;&#29983;&#12463;&#12523;&#12540;&#12474;&#12391;&#20182;&#12398;&#30058;&#32068;&#12434;&#25506;&#12377;</name><url>http://live.nicovideo.jp/cruise</url></item></adsense></getstreaminfo>'
 
-# 
 class UnexpectedStatusError(Exception):
     def __init__(self, status="n/a"):
         self.status = status
@@ -31,7 +27,7 @@ class UnexpectedStatusError(Exception):
     def __str__(self):
         return 'unexpected status "%s" found.' % self.status
 
-#
+
 class NicoAlert():
 # class lifecycle
     def __init__(self):
@@ -59,8 +55,8 @@ class NicoAlert():
         mail = config.get("nicoalert", "mail")
         password = config.get("nicoalert", "password")
         try:
-            target_communities = config.get("nicoalert",
-                "target_communities").split(',')
+            target_communities = config.get(
+                "nicoalert", "target_communities").split(',')
         except ConfigParser.NoOptionError, unused_error:
             target_communities = None
 
@@ -79,8 +75,8 @@ class NicoAlert():
         access_key = config.get("twitter", "access_key")
         access_secret = config.get("twitter", "access_secret")
 
-        self.logger.debug("consumer_key: %s consumer_secret: ***"
-            "access_key: %s access_secret: ***" %
+        self.logger.debug(
+            "consumer_key: %s consumer_secret: *** access_key: %s access_secret: ***" %
             (consumer_key, access_key))
 
         auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -121,7 +117,7 @@ class NicoAlert():
 
 # nico
     def get_ticket(self):
-        query = {'mail':self.mail, 'password':self.password}
+        query = {'mail': self.mail, 'password': self.password}
         res = urllib2.urlopen(
             'https://secure.nicovideo.jp/secure/login?site=nicolive_antenna',
             urllib.urlencode(query))
@@ -131,7 +127,7 @@ class NicoAlert():
         # self.logger.debug(etree.tostring(res_data))
         # sample response
         #{'nicovideo_user_response': {'status': {'value': 'ok'},
-        #                             'ticket': {'value': 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'},
+        #                             'ticket': {'value': 'xxxx'},
         #                             'value': '\n\t'}}
 
         ticket = res_data.xpath("//ticket")[0].text
@@ -140,9 +136,9 @@ class NicoAlert():
         return ticket
 
     def get_alert_status(self, ticket):
-        query = {'ticket':ticket}
-        res = urllib2.urlopen('http://live.nicovideo.jp/api/getalertstatus',
-            urllib.urlencode(query))
+        query = {'ticket': ticket}
+        res = urllib2.urlopen(
+            'http://live.nicovideo.jp/api/getalertstatus', urllib.urlencode(query))
 
         res_data = etree.fromstring(res.read())
         # self.logger.debug(etree.tostring(res_data))
@@ -161,7 +157,7 @@ class NicoAlert():
         #                    'user_prefecture': {'value': '12'},
         #                    'user_sex': {'value': '1'}}}
         # if res_data.getalertstatus.status != 'ok' :
-        if status != 'ok' :
+        if status != 'ok':
             raise UnexpectedStatusError(status)
 
         # create mycommunities
@@ -178,8 +174,7 @@ class NicoAlert():
         return communities, host, port, thread
 
     def get_stream_info(self, stream_id):
-        res = urllib2.urlopen('http://live.nicovideo.jp/api/getstreaminfo/lv'+
-            stream_id)
+        res = urllib2.urlopen('http://live.nicovideo.jp/api/getstreaminfo/lv' + stream_id)
         xml = res.read()
         # xml = test_stream_info_normal
         # xml = test_stream_info_no_title
@@ -188,7 +183,7 @@ class NicoAlert():
 
         # sample response
         #{'getstreaminfo': {'adsense': {'item': {'name': {'value': u'xxxxxxx'},
-        #                                        'url': {'value': 'http://live.nicovideo.jp/cruise'}}},
+        #                         'url': {'value': 'http://live.nicovideo.jp/cruise'}}},
         #                   'communityinfo': {'name': {'value': u'xxxxxxx'},
         #                                     'thumbnail': {'value': 'http://xxxxxxx'}},
         #                   'request_id': {'value': 'lv75933298'},
@@ -204,8 +199,10 @@ class NicoAlert():
             community_name = res_data.xpath("//getstreaminfo/communityinfo/name")[0].text
             stream_title = res_data.xpath("//getstreaminfo/streaminfo/title")[0].text
             # set "n/a", when no value provided; like <title/>
-            if community_name is None: community_name = "n/a"
-            if stream_title is None: stream_title = "n/a"
+            if community_name is None:
+                community_name = "n/a"
+            if stream_title is None:
+                stream_title = "n/a"
         else:
             raise UnexpectedStatusError(status)
 
@@ -253,8 +250,8 @@ class NicoAlert():
             sys.exit()
 
         if self.target_communities is None:
-            self.logger.debug("target communities is not specified, "
-                "so use my communities in my account.")
+            self.logger.debug(
+                "target communities is not specified, so use my communities in my account.")
             self.target_communities = communities
         self.logger.debug("target communities: %s" % self.target_communities)
 
@@ -262,14 +259,13 @@ class NicoAlert():
         # self.schedule_stream_stat_timer()
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((host, port))
-        sock.sendall(('<thread thread="%s" version="20061206" res_form="-1"/>'
-            +chr(0)) % thread)
+        sock.sendall(('<thread thread="%s" version="20061206" res_form="-1"/>' + chr(0)) % thread)
 
         msg = ""
-        while True :
+        while True:
             rcvmsg = sock.recv(1024)
             for ch in rcvmsg:
-                if ch == chr(0) :
+                if ch == chr(0):
                     # res_data = xml.fromstring(msg)
                     res_data = etree.fromstring(msg)
                     # sample response
@@ -291,7 +287,7 @@ class NicoAlert():
                         thread = res_data.xpath("//tread")
                         if thread:
                             self.logger.debug("番組通知の受信を開始しました．")
-        
+
                         # 'chat'
                         chat = res_data.xpath("//chat")
                         if chat:
@@ -303,7 +299,7 @@ class NicoAlert():
                     except KeyError:
                         self.logger.debug("その他のデータを受信しました．")
                     msg = ""
-                else :
+                else:
                     msg += ch
 
 if __name__ == "__main__":
